34986adafe449b1291cb896f96981c672eeeadb8
jdgreenfield <jacob.d.greenfield@gmail.com>
2014-05-05 21:33:47 -0600

    example 2 complete
    So here is the completed example of example 2

--- 2.DOM Manipulation/script.js
+++ 2.DOM Manipulation/script.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function(){
+	$('p').css('color','red');
+	$('p').css('background-color','#ffff00');
+	$('p').css({'border-style': 'dashed', 'border-width': '2px'});
+	$('.sandbox li:first-child').text("cool")
+});
