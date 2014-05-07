4c6c4c52dec72107447fc523329ead9774ee1c07
jdgreenfield <jacob.d.greenfield@gmail.com>
2014-05-06 21:33:34 -0600

    exercise 4 completed
    Here is the completed version of example 4

--- 4.Changing the DOM/script.js
+++ 4.Changing the DOM/script.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
+	$('.removeMe p').empty();
+	console.log('Step 1');
+	$('.removeMe p').remove();
+	console.log('Step 2');
+	$('.removeMe').remove();
+	console.log('Step 3');
+	$('.sandbox').append('<div class="target"></div>');
+	console.log('Step 4');
+	$('.target').append('<p></p>');
+	console.log('Step 5');
+	$('.target p').append('Hello World!');
+	console.log('Step 6');
 
+	});
