469bf8b34ab350be8fedcd39a62466d53b451ae7
jdgreenfield <jacob.d.greenfield@gmail.com>
2014-05-06 19:34:33 -0600

    example 3 completed
    Here is the completed version of example 3

--- 3.DOM Traversal/script.js
+++ 3.DOM Traversal/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-	var word = $('.sample').children('p:first').children('.targetText').text(
-		$('.sample').children('.p:first').children('.newText').text(word)
+	var word = $('.sample').children('.firstParagraph').children('.targetText').text();
+	console.log(word);
+	$('.sample').children('.firstParagraph').children('.nextText').text(word);
+	$('.sample').children('.emptyText').text(word);
 
-		);
-	})
+	});
