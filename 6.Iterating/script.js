ac98abbe9a68481e8d90bb83110d3b0ac7e9ddd8
jdgreenfield <jacob.d.greenfield@gmail.com>
2014-05-07 21:42:19 -0600

    Ex 6 Completed and Ex 7 Partial
    Completed example 6, and started example 7

--- 6.Iterating/script.js
+++ 6.Iterating/script.js
@@ -1,1 +1,7 @@
+$(document).ready(function(){
+	$('.sandbox ul li:nth-child(even)').css('color','black');
+	console.log('Step 1');
+	$('.sandbox ul li:nth-child(odd)').css('color','gray');
+	console.log('Step 2');
 
+});
