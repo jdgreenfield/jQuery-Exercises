7e30fa0dce0c17c08d94857050fab397bed4c568
jdgreenfield <jacob.d.greenfield@gmail.com>
2014-05-08 16:35:39 -0600

    Exercise 7 Completed
    This is the competed version of exercise 7

--- 7.Clone,Add,Count/script.js
+++ 7.Clone,Add,Count/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+	$('.add-item').click(function(){
+		var x = $('#numberToAdd').val();
+		for(i=1;i<=x;i++){
+			$('.template li').clone().text('text' + i).appendTo('.sample-list');
+			$('.count').text(i);
+			console.log('1st Step');
+		};
+
+	});
 	
+
 });
