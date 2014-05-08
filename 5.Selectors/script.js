bacfd3f0670304db8e5cb4ffd1fb573324bc640d
jdgreenfield <jacob.d.greenfield@gmail.com>
2014-05-07 21:18:35 -0600

    Exercise 5 Complete
    Here is the completed version of exercise 5

--- 5.Selectors/script.js
+++ 5.Selectors/script.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
 	$('.sandbox p:nth-child(2)').css('color','red');
 	console.log('1st Step');
 	$('.sandbox p:nth-child(1)').css('background-color','red');
+	console.log('2nd Step');
+	$('.sandbox p:nth-child(3)').remove();
+	console.log('3rd Step');
+	$('.sandbox ul li:nth-child(4)').css('color','blue');
+	console.log('4th Step');
+	$('.sandbox ul li:nth-child(2)').text('I did it!');
+	console.log('5th Step');
 
 
 });
